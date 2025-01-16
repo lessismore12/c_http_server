10eaae73549365def365b64b77cb5585dea032a8
Andrew Less <andrew.less22@gmail.com>
2025-01-15 22:13:25 -0500

    made content_length dynamic and naming

--- server.c
+++ server.c
@@ -1,20 +1,11 @@
-//#ifdef _WIN32
-    #include <winsock2.h>
-    #include <ws2tcpip.h>
-    typedef SOCKET Socket;
-    #define CLOSESOCKET closesocket
-/* #else
-    #include <sys/socket.h>
-    #include <netinet/in.h>
-    #include <arpa/inet.h>
-    #include <unistd.h>
-    typedef int Socket;
-    #define CLOSESOCKET close
-#endif */
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <stdio.h>
-#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
 
-
+#pragma comment(lib, "Ws2_32.lib")
+#define CLOSESOCKET closesocket
 #define PORT 8080
 #define BUFFER_SIZE 4096
 
@@ -26,159 +17,122 @@ struct Response {
     char body[256];
 };
 
-void to_string(struct Response* response, char *res) 
-{
-
+void to_string(struct Response* response, char* res) {
     strcpy(res, response->httpType);
     strcat(res, response->contentType);
     strcat(res, response->contentLength);
     strcat(res, response->connection);
     strcat(res, response->body);
-
 }
 
-void generate_http_response(char* body, char* res) 
-{
-
+void generate_http_response(const char* body, char* res) {
     struct Response responseStruct;
 
-    strcpy(responseStruct.httpType, "HTTP/1.1 200 OK\r\n");
-    strcpy(responseStruct.contentType, "Content-Type: text/html\r\n");
-    strcpy(responseStruct.contentLength, "Content-Length: 52\r\n");
-    strcpy(responseStruct.connection, "Connection: close\r\n\r\n");
+    sprintf(responseStruct.httpType, "HTTP/1.1 200 OK\r\n");
+    sprintf(responseStruct.contentType, "Content-Type: text/html\r\n");
+    sprintf(responseStruct.contentLength, "Content-Length: %zu\r\n", strlen(body));
+    sprintf(responseStruct.connection, "Connection: close\r\n\r\n");
     strcpy(responseStruct.body, body);
 
     to_string(&responseStruct, res);
 }
 
-void handle_client(SOCKET clientSocket) 
-{
-
-    char *buffer = malloc(BUFFER_SIZE);
-    int bytesRead = recv(clientSocket, buffer, BUFFER_SIZE - 1, 0); // Reserve space for null-terminator
-
-    if (bytesRead == 0) 
-    {
-        printf("Connection closed by client.\n");
-        free(buffer);
-        closesocket(clientSocket);
-        return;
-    } 
-    else if (bytesRead == SOCKET_ERROR) 
-    {
-        printf("recv failed: %d\n", WSAGetLastError());
-        free(buffer);
-        closesocket(clientSocket);
+void handle_client(SOCKET clientSocket) {
+    char* buffer = malloc(BUFFER_SIZE);
+    if (!buffer) {
+        perror("Failed to allocate memory for buffer");
+        CLOSESOCKET(clientSocket);
         return;
     }
 
-    buffer[bytesRead] = '\0'; // Null-terminate only if bytesRead > 0
+    int bytesRead = recv(clientSocket, buffer, BUFFER_SIZE, 0);
+    if (bytesRead == SOCKET_ERROR) {
+        printf("recv failed: %d\n", WSAGetLastError());
+        free(buffer);
+        CLOSESOCKET(clientSocket);
+        return;
+    }
 
-    if (bytesRead > 0) 
-    {
+    buffer[bytesRead] = '\0'; // Null-terminate the buffer
 
-        char *response = malloc(512);
-
-        if (response == NULL) 
-        {
+    if (bytesRead > 0) {
+        char* response = malloc(BUFFER_SIZE);
+        if (!response) {
             perror("Failed to allocate memory for response");
-          //  #ifdef _WIN32
-           //     WSACleanup();
-           // #endif
-            closesocket(clientSocket);
+            free(buffer);
+            CLOSESOCKET(clientSocket);
             return;
         }
-        
-        response[0] = '\0'; // Initialize the string to be empty
-        char *body = "<html><body><h1>Hi Katie!</h1></body></html>";
-        printf("Request received:\n");
-        printf("%s\n", buffer);
-        generate_http_response(body, response);
-        
-        if (response == NULL) 
-        {
-            printf("Response is null\n");
-        }
 
-        printf("Response:\n");
-        printf("%s\n", response);
+        response[0] = '\0'; // Initialize the string to be empty
+        const char* body = "<html><body><h1>Hi Katie!</h1></body></html>";
+        printf("Request received:\n%s\n", buffer);
+
+        generate_http_response(body, response);
+        printf("Response:\n%s\n", response);
 
         send(clientSocket, response, strlen(response), 0);
 
         free(response);
-
-    } else 
-    {
+    } else {
         printf("recv failed or connection closed by client.\n");
     }
-    
+
     free(buffer);
-    closesocket(clientSocket);
+    CLOSESOCKET(clientSocket);
 }
 
-int main() 
-{
-
-    Socket server_socket;
+int main() {
+    SOCKET server_socket;
     struct sockaddr_in server_addr, client_addr;
 
-//#ifdef _WIN32
     WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0 )
-    {
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         printf("WSAStartup failed\n");
         return 1;
     }
-//#endif
 
     server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (server_socket == INVALID_SOCKET)
-    {
-        printf("socket creation failed");
+    if (server_socket == INVALID_SOCKET) {
+        printf("Socket creation failed: %d\n", WSAGetLastError());
         WSACleanup();
         return 1;
     }
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);    
+    server_addr.sin_port = htons(PORT);
 
-    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
-    {
+    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
         printf("Bind failed: %d\n", WSAGetLastError());
-        closesocket(server_socket);
+        CLOSESOCKET(server_socket);
         WSACleanup();
         return 1;
     }
 
-    if (listen(server_socket, SOMAXCONN) == SOCKET_ERROR)
-    {
+    if (listen(server_socket, SOMAXCONN) == SOCKET_ERROR) {
         printf("Listen failed: %d\n", WSAGetLastError());
-        closesocket(server_socket);
-      //  #ifdef _Win32
-            WSACleanup();
-      //  #endif
+        CLOSESOCKET(server_socket);
+        WSACleanup();
         return 1;
     }
-    
+
     printf("Server listening on port %d\n", PORT);
 
-    while (true)
-    {
+    while (1) {
         struct sockaddr_in clientAddr;
         int clientAddrSize = sizeof(clientAddr);
         SOCKET clientSocket = accept(server_socket, (struct sockaddr*)&clientAddr, &clientAddrSize);
+
         if (clientSocket == INVALID_SOCKET) {
-            printf("Accept failed: : %d\n", WSAGetLastError());
+            printf("Accept failed: %d\n", WSAGetLastError());
             continue;
         }
 
         handle_client(clientSocket);
     }
-    
-    closesocket(server_socket);
-   // #ifdef _WIN32
-        WSACleanup();
-    //#endif
+
+    CLOSESOCKET(server_socket);
+    WSACleanup();
     return 0;
 }
